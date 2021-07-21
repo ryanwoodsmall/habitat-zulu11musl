c6821755feba1162f10e440665b386b680bf1a4e
ryanwoodsmall <rwoodsmall@gmail.com>
2021-07-21 01:54:40 +0000

    update to 11.0.12

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_origin="ryanwoodsmall"
 pkg_name="zulu11musl"
 pkg_maintainer="ryanwoodsmall <rwoodsmall@gmail.com>"
-ver1="11.0.11"
-ver2="11.48.21"
+ver1="11.0.12"
+ver2="11.50.19"
 pkg_version="${ver1}.${ver2}"
 pkg_dirname="zulu${ver2}-ca-jdk${ver1}-linux_musl_x64"
 pkg_filename="${pkg_dirname}.tar.gz"
 pkg_source="https://cdn.azul.com/zulu/bin/${pkg_filename}"
-pkg_shasum="406b76e53e8be7e19436dee6954ee903f50c0a2ed3ed86456de4c7b5072aa544"
+pkg_shasum="231073c8c07c8aa4044abcfabe28e28ee9f5eec513a5da59e87d1f6ea41fa7f8"
 pkg_license=( "GPL-2.0-only WITH Classpath-exception-2.0" )
 pkg_description=( 'Zulu is a build of the Open Java Development Kit (OpenJDK) on musl libc with long-term support from Azul' )
 pkg_upstream_url="https://www.azul.com/downloads/zulu-community/"
